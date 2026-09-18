a21f00fde17d56ffcd268ed10049901c5c7b4c05
Angelica <229909157+angiesson@users.noreply.github.com>
2026-09-18 23:37:49 +0200

    Add new product page and navigation

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './routes/home/home';
 import { Productdetails } from './routes/productdetails/productdetails';
 import { Products } from './routes/admin/products/products';
+import { NewProduct } from './routes/admin/new-product/new-product';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'products/:id', component: Productdetails },
-  { path: 'admin/products', component: Products }
+  { path: 'admin/products', component: Products },
+  { path: 'admin/products/new', component: NewProduct }
 ];
---
+++ client/src/app/routes/admin/new-product/new-product.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-new-product',
+  styleUrl: './new-product.css',
+  templateUrl: './new-product.html',
+})
+export class NewProduct {}
