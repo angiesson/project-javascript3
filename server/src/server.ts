eba9f4b010b08a2b8a96b08eafaffaed740a4686
Angelica <229909157+angiesson@users.noreply.github.com>
2026-09-21 16:28:32 +0200

    Add SQLite product database

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { products } from "./data/products.js";
+import db from './database.js';
 
 // Skapa expressapplikation
 const app = express();
@@ -9,18 +9,27 @@ app.use(express.json());
 // Bestäm vilken port servern ska använda
 const port = process.env.PORT || 8000;
 
-// Skapa endpoints som svarar på HTTP GET för api/products
+// Hämta alla produkter från databasen
 app.get("/api/products", (req, res) => {
+    const products = db
+    .prepare('SELECT * FROM products')
+    .all();
   res.json(products);
 });
 
 app.post("/api/products", (req, res) => {
-  const newProduct = {
-    id: products.length + 1,
-    ...req.body,
-  };
+  const { name, description, price, sku, imageUrl } = req.body;
 
-  products.push(newProduct);
+  const result = db
+  .prepare(`
+    INSERT INTO products (name, description, price, sku, imageUrl)
+    VALUES (?, ?, ?, ?, ?)
+  `)
+  .run(name, description, price, sku, imageUrl);
+
+  const newProduct = db
+  .prepare('SELECT * FROM products WHERE id = ?')
+  .get(result.lastInsertRowid);
 
   res.status(201).json(newProduct);
 });
@@ -30,7 +39,9 @@ app.post("/api/products", (req, res) => {
 app.get("/api/products/:id", (req, res) => {
   const id = Number(req.params.id);
 
-  const product = products.find((product) => product.id === id);
+    const product = db
+    .prepare('SELECT * FROM products WHERE id = ?')
+    .get(id);
 
   res.json(product);
 });
