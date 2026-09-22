bfed8f5f059c6bbed51293bd970da1ba9c5010ca
Angelica <229909157+angiesson@users.noreply.github.com>
2026-09-22 17:10:46 +0200

    Add header with EverPure logo

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Search } from './search/search';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Search],
+  imports: [RouterOutlet, Search, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
