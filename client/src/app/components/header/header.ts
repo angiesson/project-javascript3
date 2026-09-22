bd207aaea15cecca7581308d11c64819db016ec8
Angelica <229909157+angiesson@users.noreply.github.com>
2026-09-22 22:39:15 +0200

    Improve search functionality and responsive header

--- client/src/app/components/header/header.ts
+++ client/src/app/components/header/header.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Search } from '../../search/search';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, Search],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
--- client/src/app/routes/search-results/search-results.ts
+++ client/src/app/routes/search-results/search-results.ts
@@ -18,19 +18,20 @@ export class SearchResults {
   products = signal<Product[]>([]);
   searchTerm = signal('');
 
-  ngOnInit() {
-    const query = this.route.snapshot.queryParamMap.get('q');
+ngOnInit() {
+  this.route.queryParamMap.subscribe(params => {
+    const query = params.get('q') ?? '';
 
-  if (query) {
     this.searchTerm.set(query);
-  }
-  this.productService.getProducts()
-  .subscribe(products => {
-    const filteredProducts = products.filter(product =>
-      product.name.toLowerCase().includes(this.searchTerm().toLowerCase())
-    );
 
-    this.products.set(filteredProducts);
+    this.productService.getProducts()
+      .subscribe(products => {
+        const filteredProducts = products.filter(product =>
+          product.name.toLowerCase().includes(query.toLowerCase())
+        );
+
+        this.products.set(filteredProducts);
+      });
   });
 }
 }
